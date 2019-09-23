db9720153724af88448f8b2010fd345ed552a045
João Loureiro <loureiro.jpc@gmail.com>
2019-09-23 15:29:52 +0100

    (State management) Provider: create state context with React Context API and Hooks.

---
+++ src/context/Context.js
@@ -0,0 +1,72 @@
+import React, { createContext, useReducer } from 'react';
+
+const Context = createContext();
+
+const initialState = {
+  layer: 0,
+  networks: {},
+  stations: {},
+};
+
+const actions = {
+  setActiveLayer: 'store/SET_ACTIVE_LAYER',
+  updateNetworks: 'store/UPDATE_NETWORKS',
+  updateStations: 'store/UPDATE_STATIONS',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.setActiveLayer: {
+      return {
+        ...state,
+        layer: action.payload,
+      };
+    }
+    case actions.updateNetworks: {
+      return {
+        ...state,
+        networks: action.payload,
+      };
+    }
+    case actions.updateStations: {
+      const { networkId, stations } = action.payload;
+      return {
+        ...state,
+        stations: {
+          [networkId]: {
+            ...stations,
+          },
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const changeLayer = layerId => {
+    dispatch(layerId);
+  };
+
+  const updateNetworks = networks => {
+    dispatch({ type: actions.updateNetworks, payload: networks });
+  };
+
+  const updateStations = (networkId, stations) => {
+    dispatch({ type: actions.updateStations, payload: { networkId, stations } });
+  };
+
+  const defaultContext = {
+    state,
+    changeLayer,
+    updateNetworks,
+    updateStations,
+  };
+
+  return <Context.Provider value={defaultContext}>{children}</Context.Provider>;
+};
+
+export { Context, ContextProvider };
